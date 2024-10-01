231f3ea30fc661c5326a43042a7eacc75e95bfc6
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-10-01 23:47:55 +0530

    before making  redux

---
+++ components/redux/Action.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Action = () => {
+    return (
+        <View>
+            <Text>Action</Text>
+        </View>
+    )
+}
+
+export default Action
+
+const styles = StyleSheet.create({})
